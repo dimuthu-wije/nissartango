689d1458e8aac1a278ce94e3ee89f0947fc1b545
dimuthu-wije <dimuthu.wije@outlook.com>
2026-08-23 17:42:08 +0200

    Add Cloudflare adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import cloudflare from '@astrojs/cloudflare';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://nissartango.fr',
+
   i18n: {
     defaultLocale: 'fr',
     locales: ['fr', 'en'],
     routing: { prefixDefaultLocale: false },
   },
+
+  adapter: cloudflare(),
 });
